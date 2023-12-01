00946f805c0772ac0ac7b664c0e761597dc59039
hardi-yansah <hardi.yansah97@gmail.com>
2023-12-01 10:49:16 +0700

    Update func test #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -99,9 +99,15 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
